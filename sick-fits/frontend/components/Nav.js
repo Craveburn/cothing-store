6b5d0e92e64d9aeadcc9891ec4b6ae67ea0e5a46
Craveburn <raburncameron@gmail.com>
2019-01-19 00:14:27 -0700

    got the boilerlplate put together using stateless components and next

---
+++ sick-fits/frontend/components/Nav.js
@@ -0,0 +1,13 @@
+import Link from "next/link";
+
+const Nav = () => (
+  <div>
+    <Link href="/sell">
+      <a>SELL!</a>
+    </Link>
+    <Link href="/">
+      <a>HOME!</a>
+    </Link>
+  </div>
+);
+export default Nav;
