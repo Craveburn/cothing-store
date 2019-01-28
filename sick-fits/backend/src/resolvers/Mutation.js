5bfa37b0228c8dfd965b6880744edbfc6f42c277
Craveburn <raburncameron@gmail.com>
2019-01-28 10:15:25 -0700

    messing around with Query and Mutations.

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,3 +1,12 @@
-const mutations = {};
+const mutations = {
+  createDog(parnts, args, ctx, info) {
+    global.dogs = global.dogs || [];
+    //create a dog!!
+    const newDog = { name: args.name };
+    global.dogs.push(newDog);
+    return newDog
+    console.log(args);
+  }
+};
 
 module.exports = mutations;
